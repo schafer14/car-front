35b0889ffc9bf15dee4b5508ff19d69c56847701
Banner <banner.schafer@gmail.com>
2014-07-20 16:08:18 +0800

    port

--- config/env/all.js
+++ config/env/all.js
@@ -6,7 +6,7 @@ module.exports = {
 		description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
 		keywords: 'MongoDB, Express, AngularJS, Node.js'
 	},
-	port: process.env.PORT || 40002,
+	port: process.env.PORT || 4002,
 	templateEngine: 'swig',
 	sessionSecret: 'MEAN',
 	sessionCollection: 'sessions',
